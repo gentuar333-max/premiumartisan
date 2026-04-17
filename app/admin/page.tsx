63504254d77d731c01580c71d092caec6dea3232
Gentuar <gentuar333@gmail.com>
2026-04-17 21:36:58 +0200

    fix: admin force-dynamic

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+export const dynamic = "force-dynamic";
 import { useEffect, useMemo, useState } from "react";
 import { supabase } from "../publier-projet/supabaseClient";
 import { formatPhone } from "@/lib/formatPhone";
