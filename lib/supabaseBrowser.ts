4a5639b7a7709f056bf6ec6524d3074786ab981c
Gentuar <gentuar333@gmail.com>
2026-04-27 11:20:30 +0200

    fix: persistent session 1 year

--- lib/supabaseBrowser.ts
+++ lib/supabaseBrowser.ts
@@ -1,8 +1,21 @@
+// lib/supabaseBrowser.ts
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createSupabaseBrowserClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        persistSession: true,
+        autoRefreshToken: true,
+        detectSessionInUrl: true,
+      },
+      cookieOptions: {
+        maxAge: 60 * 60 * 24 * 365, // 1 vit
+        sameSite: "lax",
+        secure: true,
+      },
+    }
   );
 }
